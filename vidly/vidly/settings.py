a96160ae2fbfb0f8732471ec1159f500df24f9c4
iheanyi.okwara <okwaraiheanyi@gmail.com>
2025-11-12 10:46:52 +0000

    Adding Homepage

--- vidly/vidly/settings.py
+++ vidly/vidly/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies.apps.MoviesConfig',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+
 ]
 
 MIDDLEWARE = [
